ed3e4b71f96559c3704e290f8c5110670e65ec4b
Charlie Cohen <linzellart@gmail.com>
2022-03-21 13:23:44 +0100

    Update

--- exercice/25-realistic-render/src/script.js
+++ exercice/25-realistic-render/src/script.js
@@ -20,9 +20,21 @@ const scene = new THREE.Scene()
  */
 const testSphere = new THREE.Mesh(
     new THREE.SphereGeometry(1, 32, 32),
-    new THREE.MeshBasicMaterial()
+    new THREE.MeshStandardMaterial()
 )
 scene.add(testSphere)
+
+/* 
+* Lights
+ */
+const directionalLight = new THREE.DirectionalLight(0xffffff, 1)
+directionalLight.position.set(0.25, 3, - 2.25)
+scene.add(directionalLight)
+
+gui.add(directionalLight, 'intensity').min(0).max(10).step(0.001).name('Intensité de la lumière')
+gui.add(directionalLight.position, 'x').min(-5).max(5).step(0.001).name('Position X')
+gui.add(directionalLight.position, 'y').min(-5).max(5).step(0.001).name('Position Y')
+gui.add(directionalLight.position, 'z').min(-5).max(5).step(0.001).name('Position Z')
 
 /**
  * Sizes
